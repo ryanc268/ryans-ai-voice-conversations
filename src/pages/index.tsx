61840eddb80425622e1b0024cb4d0254b32d21c7
Ryan Coppa <ryan.tcoppa@gmail.com>
2023-03-07 22:09:43 -0500

    Better "Powered By" styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,21 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { motion } from "framer-motion";
 
 import Link from "next/link";
 import Image from "next/image";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesDown, faAnglesUp } from "@fortawesome/free-solid-svg-icons";
+
 import azureCognitive from "/public/microsoft_cognitive_banner.png";
 import gptBanner from "/public/chatgpt_banner.png";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <Head>
@@ -41,25 +48,43 @@ const Home: NextPage = () => {
           <div className="flex flex-col items-center gap-2">
             <AuthShowcase />
           </div>
-          <div className="absolute bottom-0 flex w-full flex-col items-center justify-center gap-4 bg-zinc-800 p-2">
-            <h2 className="text-lg md:text-2xl">Powered By:</h2>
-            <div className="flex items-center justify-center gap-8">
-              <Image
-                className="w-28 md:w-40"
-                src={azureCognitive}
-                width={0}
-                height={0}
-                alt="Logo"
+          <motion.div
+            layout
+            className="absolute bottom-0 flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-zinc-800 p-2 shadow-inner shadow-zinc-500"
+            onClick={() => setToggle(!toggle)}
+          >
+            <motion.div
+              layout
+              className="flex cursor-pointer items-center justify-center gap-4"
+            >
+              <h2 className="text-lg md:text-2xl">Powered By:</h2>
+              <FontAwesomeIcon
+                className="w-4 md:w-6"
+                icon={toggle ? faAnglesDown : faAnglesUp}
               />
-              <Image
-                className="w-28 md:w-40"
-                src={gptBanner}
-                width={0}
-                height={0}
-                alt="Logo"
-              />
-            </div>
-          </div>
+            </motion.div>
+            {toggle && (
+              <motion.div
+                layout
+                className="flex items-center justify-center gap-8"
+              >
+                <Image
+                  className="w-28 md:w-40"
+                  src={azureCognitive}
+                  width={0}
+                  height={0}
+                  alt="Logo"
+                />
+                <Image
+                  className="w-28 md:w-40"
+                  src={gptBanner}
+                  width={0}
+                  height={0}
+                  alt="Logo"
+                />
+              </motion.div>
+            )}
+          </motion.div>
         </div>
       </main>
     </>
